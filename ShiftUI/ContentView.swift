62a1325c9135719c048fb0ca8fd6b6b73a59552c
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-02-19 11:58:40 -0500

    Initial Commit

---
+++ ShiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShiftUI
+//
+//  Created by Ryan Lintott on 2021-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShiftUI/ShiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftUIApp.swift
+//  ShiftUI
+//
+//  Created by Ryan Lintott on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ShiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
