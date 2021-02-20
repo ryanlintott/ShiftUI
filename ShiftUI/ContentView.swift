d146a42d5be9299bb38073fb2ab6f3fb61803833
zoha131 <abirhasanzoha@gmail.com>
2021-02-20 15:27:53 +0600

    Implement Basic Neumorphism

--- ShiftUI/ContentView.swift
+++ ShiftUI/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      ZStack {
+        Color.offWhite.edgesIgnoringSafeArea(.all)
+
         BoardView()
+      }
     }
 }
 
